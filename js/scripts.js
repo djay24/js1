5513fa627f0ce3023f06ec993aad6975e5e28332
Dj Rodriguez <djrodriguez@Djs-MacBook-Air.local>
2018-04-10 16:53:43 -0700

    added takehome console log

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
 var myHistory = "I am in this class to continue to grow as a developer";
 console.log(myHistory);
+var myHobbies = "Some of my hobbies include playing video games, and playing basketball";
+console.log(myHobbies);
 //console.log(myHistory)
 var myValue = confirm(myHistory);
 console.log(myValue)
