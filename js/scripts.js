6b2b580736866bee391d9c7689ffb5b520419e51
Dj Rodriguez <djrodriguez@Djs-MacBook-Air.local>
2018-04-11 20:58:44 -0700

    day 2 work

---
+++ js/scripts.js
@@ -0,0 +1,45 @@
+var myName = "DJ"; // declared and defined
+// myName = new String(myName);
+//console.log(myName, "One");
+
+
+/*var test; //declared
+test = 'My test is that i am awesome'; //defined
+console.log(test); */
+
+{
+  let myName = "Franklin";
+  //console.log(myName, "Two");
+
+};
+//console.log(myName, "Three");
+
+let birthday = "11/28/96"
+let rightNow = new Date(birthday);
+
+//alert(rightNow);
+
+//math object
+let myString = "Hello World!";
+//alert(myString.length);
+
+//array example
+let myArray = ["Sam", 4, new Date()];
+
+
+let myObject = {
+  studentCount: 10,
+  teacherRating: 100000
+};
+
+//alert, 1 option just to tell user something
+//alert('Hey, you forgot your socks.');
+
+
+//confirm
+//confirm("Are you 21 or older?");
+//const isOfAge = confirm("are you 21 or older?");
+
+//prompt
+const userStory = prompt("Do you take walks?");
+console.log(userStory)
