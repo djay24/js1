93ac9f351add6defacf518c77d8a324a4ed0f9a3
Dj Rodriguez <djrodriguez@Djs-MacBook-Air.local>
2018-04-13 15:40:39 -0700

    added prompt questions for day 2

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+var firstName = prompt("What is your First Name?");
+console.log(firstName);
+var lastName = prompt("Hey " +firstName + " what is your last name?");
+console.log(lastName);
+var yourBirthday = prompt("Hey " +firstName + " when was your birthday? (MM/DD/YYYY) format");
+console.log(yourBirthday);
+var birthdayAlert = new Date(yourBirthday);
+
+alert(birthdayAlert);
