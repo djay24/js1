0060f470ac52ce3ec59d5190b86eaf3dbc263fb5
Dj Rodriguez <djrodriguez@Djs-MacBook-Air.local>
2018-04-16 18:36:50 -0700

    added classnotes

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,16 @@
-var firstName = prompt("What is your First Name?");
+var firstName = prompt("What is your First Name?" , 'Franklin');
 console.log(firstName);
+// if you put , after first string like this
+//("what is your first name", "charles") the charles will be inputed if there is no value input
 var lastName = prompt("Hey " +firstName + " what is your last name?");
 console.log(lastName);
 var yourBirthday = prompt("Hey " +firstName + " when was your birthday? (MM/DD/YYYY) format");
 console.log(yourBirthday);
 var birthdayAlert = new Date(yourBirthday);
 
-confirm("Is this your birthday? " +birthdayAlert);
+var birthdayConfirm = confirm("Is this your birthday? " +yourBirthday);
+console.log(birthdayConfirm);
+
+
+//let myName = "DJ"
+//alert (`Hello ${myName}. You are great!`);
