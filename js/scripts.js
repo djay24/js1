aa1210fc2e0b3f29b9c4d9b741b7e75d1a9e89fb
Dj Rodriguez <djrodriguez@Djs-MacBook-Air.local>
2018-04-18 19:26:58 -0700

    added notes

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+// feilds
+let emailArr;
+var yourEmail;
+while (true) {
+  //user input
+  var yourEmail = prompt('What is your email?');
+  emailArr = yourEmail.split("");
+  if (!(emailArr.includes('@'))) {
+    var yourEmail = prompt('That was not a valid email. Please try again.');
+  }
+  else if (!(emailArr.includes('.'))) {
+    var yourEmail = prompt('That was not a valid email. Please try again.');
+  }
+  else {
+    alert('Thanks for providing a valid email.');
+  }
+}
