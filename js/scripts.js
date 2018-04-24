288d8d3d02030cdb9f5d6a8a53fc440ec9bfa049
Dj Rodriguez <djrodriguez@Djs-MacBook-Air.local>
2018-04-23 20:58:07 -0700

    started notes for functions

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+//console.log("Hello World"); // method which is a type of function
+function sayHello(name, greeting) { //function definition
+  return `Hello ${name}, ${greeting}`
+}
+
+let myGreeting = sayHello("DJ", "You're the greatest"); // call site
+
+console.log(myGreeting);
