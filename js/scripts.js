d819d9cd6591a1691ffd2d771d59ebd5dfeccd9d
Dj Rodriguez <djrodriguez@Djs-MacBook-Air.local>
2018-04-24 16:01:06 -0700

    started to do array, trying to figure out how to list out items

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+// starting to-do array
+let item1 = prompt("List your fist thing you need to do tommorrow");
+let item2 = prompt("Please list a second thing you need to do")
+let item3 = prompt("Now a third")
+
+let myTodo = [];
+myTodo.push(item1, item2, item3);
+
+console.log(myTodo);
+
+for(i = 0; i < myTodo.length; i++) {
+  console.log(i, myTodo[0])
+}
