fc9a891378110e0466814b34f8d73f023daf1920
Dj Rodriguez <djrodriguez@Djs-MacBook-Air.local>
2018-04-26 15:28:54 -0700

    started work on function

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+let yourNum = parseInt(prompt("Give me a number?"));
+
+function squareCalc(yourNum) {
+  return yourNum * yourNum;
+}
+
+if (!isNaN(yourNum)) {
+sqareCalc(yourNum);
+alert(yourNum);
+} else {
+
+}
