42e68be159993786607150c63b4655d05d70b2fe
Dj Rodriguez <djrodriguez@Djs-MacBook-Air.local>
2018-04-25 20:48:17 -0700

    added in class functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,74 @@
-//console.log("Hello World"); // method which is a type of function
-function sayHello(name, greeting) { //function definition
-  return `Hello ${name}, ${greeting}`
-}
+// //console.log("Hello World"); // method which is a type of function
+// function sayHello(name, greeting) { //function definition
+//   return `Hello ${name}, ${greeting}`
+// }
+//
+// let myGreeting = sayHello("DJ", "You're the greatest"); // call site
+//
+// console.log(myGreeting);
 
-let myGreeting = sayHello("DJ", "You're the greatest"); // call site
+// fat arrow functions look like this and they do not have the function keyword
 
-console.log(myGreeting);
+// () =>{  // does not need to have parentheses if there is only one argument but needs them if you have more than one argument
+
+// }
+
+// (thingToLog,thingToSay) => {
+//   console.log(thingToLog);
+//   alert(thingToSay);
+// }
+
+
+// Dog years conversion
+//
+// let dogAge = parseInt(prompt("How old is your dog?", "2"));
+//
+// if (!isNaN(dogAge)) {
+//   dogYears(dogAge);
+//   alert(dogAge);
+// } else {
+//   let
+// }
+//
+// function dogYears(age) {
+//   return age * 7;
+// }
+
+// let yourAge = parseInt(prompt("How old are you?", "20"));
+//
+// let usageRate = parseInt(prompt("How many times a day do you use our product?", "2"));
+//
+// if (!isNaN(yourAge && usageRate)) {
+// lifetimeSupplyCalc(yourAge, usageRate)
+// console.log()
+// } else {
+//
+// }
+//
+// function lifetimeSupplyCalc(yourAge, usageRate) {
+//   let yearsLeft = 80 - yourAge; // How many years you have left
+//   let yearUse = usageRate * 365; // How many times a year you use
+//   let yourAmount = yearsLeft * yearUse;
+//   console.log(yourAmount)
+// }
+
+
+// ryeker solution
+// function calcLifetimeSupply(ageNow, amountPerDay) {
+//   const yearsLeft = 80 - ageNow;
+//   return yearsLeft * (365 * amountPerDay)
+// }
+//
+// let ageOfUser = parseInt(prompt("How old are you?", "32"));
+//
+// let dailyUse = parseInt(prompt("How many times a day do you use our product?", "3"))
+//
+// if(!isNaN(ageOfUser) && !isNaN(dailyUse)) {
+//   alert(calcLifetimeSupply(ageOfUser, dailyUse));
+//
+// }
+
+
+
+
+// method
