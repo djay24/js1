ba2f581f0ed1630a1dfb2fec10987a5c681e463e
Dj Rodriguez <djrodriguez@Djs-MacBook-Air.local>
2018-04-30 18:24:43 -0700

    added notes from ryekers solutions

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,28 @@
-let yourNum = parseInt(prompt("Give me a number?"));
+let yourNum = parseInt(prompt("Please give me a number?"));
 
 function squareCalc(yourNum) {
   return yourNum * yourNum;
+}  // it would help to put if else statement with the NaN prompt in the same function as the actual math function
+
+function requestData() {
+  let yourNum = parseInt(prompt("Please give me a number?"));
+  if(!isNaN(yourNum)) {
+    console.log(yourNum);
+    return yourNum;
+  } else {
+    alert("Please give a real number");
+    requestData();
+  }
 }
-
-// function giveRealNum(yourNum) {
-//   let giveNum = prompt("Please give a Number");
-//   if (!isNaN(giveNum)) {
-//     return yourNum;
-//   } else giveRealNum(yourNum);
-//}
-
+// checks if value is a number and if it isnt it goes to requestData function
 if (!isNaN(yourNum)) {
 squareCalc(yourNum);
 alert(`the square root of ${yourNum} is ${squareCalc(yourNum)}`);
 } else {
 alert("Please give a valid number");
-let realNum = parseInt(prompt("Please give a Number"));
-  
+requestData(yourNum);
+parseInt(squareCalc(yourNum));
+alert(`the square root of ${yourNum} is ${squareCalc(yourNum)}`);
 }
+
+// you can call function on a line by itself to make things easier
