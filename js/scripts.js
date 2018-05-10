bd56cc560d9c963ac98f100bee0cc18a020ac73d
Dj Rodriguez <djrodriguez@Djs-MacBook-Air.local>
2018-05-09 20:36:59 -0700

    added notes for day 10

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+
 // // javascrip timers
 // // there is setTimeout() and setInterval
 // setTimeout( // set timeout method
@@ -20,21 +21,50 @@
 //       alert("Hi")
 //     },
 //   2000);
-yourTime();
-function makeTimer(time) {
-  setTimeout(
-    () => {
-      alert("How you doin'?")
-    },
-  time)
-};
-function yourTime() {
-  let givenTime = parseInt(prompt("Set a timer in milliseconds", "1 Second = 1000 ms"));
-  if(!isNaN(givenTime)) {
-    makeTimer(givenTime);
-  } else {
-    alert("Please give a real number")
-    yourTime();
-  }
-}
-console.log();
+
+// my attempt
+// yourTime();
+// function makeTimer(time) {
+//   setTimeout(
+//     () => {
+//       alert("How you doin'?")
+//     },
+//   time)
+// };
+// function yourTime() {
+//   let givenTime = parseInt(prompt("Set a timer in milliseconds", "1 Second = 1000 ms"));
+//   if(!isNaN(givenTime)) {
+//     makeTimer(givenTime);
+//   } else {
+//     alert("Please give a real number")
+//     yourTime();
+//   }
+// }
+// console.log();
+
+
+
+// session storage
+
+// let user = {
+//   email: "djayr24@gmail.com",
+//   firstName: "DJ",
+//   lastName: "Rodriguez",
+//   cart: [],
+//   lastLogin: Date.now()
+// };
+//
+// sessionStorage.setItem("currentUser",
+// JSON.stringify(user));
+//
+// user =
+// JSON.parse(sessionStorage.getItem("currentUser"));
+//
+// let firstName = user.firstName;
+// alert(`Hi ${firstName}, nice to see you again`);
+
+// function logout() {
+//   sessionStorage.removeItem("currentUser");
+// };
+// const logoutButton = document.querySelector(".logout")
+// logoutButton.addEventListener("click", logout());
